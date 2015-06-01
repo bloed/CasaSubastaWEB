9aa2a0c91f7aa27b70e7459d6e9e75baa43f8f05
bloed <josuearrietasalas@gmail.com>
2015-06-01 17:16:32 -0600

    SET up lo basico
    Los get´s son hermosos a la hora de mostrarlos

---
+++ Subastas/Subastas/Login.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="Subastas.Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="Styles.css" rel="stylesheet" />
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+        <asp:GridView ID="GridView1" runat="server">
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ Subastas/Subastas/Login.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace Subastas
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //string connection = "data source.; database = SUBASTAS; user id =  ; password = ";//SQL Server authentication
+            string connection = "Server = PC-GAME; database = SUBASTAS; integrated security=SSPI";//windows authentication
+            // data source.   es local server
+            SqlConnection con = new SqlConnection(connection);
+            SqlCommand cmd = new SqlCommand("select * from Usuarios", con);
+            con.Open();
+            GridView1.DataSource = cmd.ExecuteReader();
+            GridView1.DataBind();
+            con.Close();
+        }
+    }
+}
